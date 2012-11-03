29d7bd8a3dd99cb580569a80b2c021cbc8bd8162
Ted John <ted@brambles.org>
2012-11-03 18:14:01 +0000

    Add tile different types as enum

---
+++ TileEngine/TileType.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Group5.TileEngine
+{
+	/// <summary>
+	/// General type and logic for a tile.
+	/// </summary>
+	enum TileType
+	{
+		Empty,
+		Solid,
+		Painful,
+		Leathal,
+		Water,
+		Fire,
+		Lava,
+		Chasm,
+		Special,
+	}
+}
