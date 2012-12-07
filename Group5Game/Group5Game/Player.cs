6c19f935fb71e1308b2ed86c1ced3fc1b6eaeb1a
dje1990 <dje1990@hotmail.com>
2012-12-07 21:34:33 +0000

    changed some movement settings

--- Group5Game/Group5Game/Player.cs
+++ Group5Game/Group5Game/Player.cs
@@ -17,7 +17,7 @@ namespace Group5.Game
         private bool move_left_pressed = false;
         private bool move_right_pressed = false;
 
-        private int human_button_delay = 500;
+        private int human_button_delay = 400;
 
         public Player(Game1 new_game)
             : this(new_game, 7, 7, 1, 1)
@@ -30,7 +30,7 @@ namespace Group5.Game
             this.inventory = new List<Item>();
             this.set_texture_key(Player.texture_key);
             this.set_movement_distance(1);
-            this.set_movement_frequency(1);
+            this.set_movement_frequency(2);
         }
 
         public void update(Game1 game, GameTime gameTime)
@@ -39,29 +39,35 @@ namespace Group5.Game
 
             Game1.input.Update();
             
-            Debug.WriteLine(this.timespan_since_last_movement.TotalMilliseconds);
             if (this.timespan_since_last_movement >= new TimeSpan(0, 0, 0, 0, this.human_button_delay))
             {
                 if (Game1.input.IsUpAction() == true)
                 {
                     this.move_up_pressed = true;
                     this.move_down_pressed = false;
+                    this.move_left_pressed = false;
+                    this.move_right_pressed = false;
                 }
                 else if (Game1.input.IsDownAction() == true)
                 {
-                    this.move_down_pressed = true;
                     this.move_up_pressed = false;
+                    this.move_down_pressed = true;
+                    this.move_left_pressed = false;
+                    this.move_right_pressed = false;
                 }
-
-                if (Game1.input.IsLeftAction() == true)
+                else if (Game1.input.IsLeftAction() == true)
                 {
+                    this.move_up_pressed = false;
+                    this.move_down_pressed = false;
                     this.move_left_pressed = true;
                     this.move_right_pressed = false;
                 }
                 else if (Game1.input.IsRightAction() == true)
                 {
-                    this.move_right_pressed = true;
+                    this.move_up_pressed = false;
+                    this.move_down_pressed = false;
                     this.move_left_pressed = false;
+                    this.move_right_pressed = true;
                 }
             }
 
@@ -83,8 +89,7 @@ namespace Group5.Game
                         this.move(Direction.Down);
                         this.move_down_pressed = false;
                     }
-
-                    if (this.move_left_pressed == true)
+                    else if (this.move_left_pressed == true)
                     {
                         this.move(Direction.Left);
                         this.move_left_pressed = false;
